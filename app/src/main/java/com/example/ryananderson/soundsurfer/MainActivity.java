21c51322fdb9137581f18b8040c95dfe31079923
rkanderson <shnoutyanderson@gmail.com>
2015-08-08 17:03:47 -0700

    cleaned up a bit

--- app/src/main/java/com/example/ryananderson/soundsurfer/MainActivity.java
+++ app/src/main/java/com/example/ryananderson/soundsurfer/MainActivity.java
@@ -17,30 +17,11 @@ public class MainActivity extends ActionBarActivity {
 
         getSupportActionBar().hide(); //Hide action bar
 
+        //TEMPORARY!
+        //Immediately goes to GameActivity
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
